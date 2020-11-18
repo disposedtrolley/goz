05f5e496467141fbd53ee00c7900ed1b120ec82e
James Liu <james_liu@fastmail.com>
2020-11-18 14:09:08 +1100

    refactor: create a lighter memory abstraction

--- internal/machine/machine.go
+++ internal/machine/machine.go
@@ -8,12 +8,12 @@ type Game []byte
 
 type Machine struct {
 	version int
-	mem     []byte
+	mem     *memory
 }
 
 func NewMachine(game Game) *Machine {
 	return &Machine{
-		mem: game,
+		mem: newMemory(game),
 	}
 }
 
@@ -24,20 +24,11 @@ func (m *Machine) WithVersion(version int) {
 func (m *Machine) Start() error {
 	if m.version == 0 {
 		// Inspect the game file for the version.
-		m.version = int(m.loadb(hVersion))
+		m.version = int(m.mem.readByte(hVersion))
 	}
 
-	fmt.Printf("z%d gamefile weighing in at %d bytes\n", m.version, len(m.mem))
-
-	fmt.Printf("beginning of static memory: %x\n", m.loadw(hStaticMemoryBegin))
+	fmt.Printf("z%d gamefile weighing in at %d bytes\n", m.version, m.mem.size())
+	fmt.Printf("beginning of static memory: %x\n", m.mem.readWord(hStaticMemoryBegin))
 
 	return nil
-}
-
-func (m *Machine) loadb(addr address) byte {
-	return m.mem[addr]
-}
-
-func (m *Machine) loadw(addr address) uint16 {
-	return uint16(m.mem[addr])<<8 | uint16(m.mem[addr+1])
 }
--- internal/machine/memory.go
+++ internal/machine/memory.go
@@ -1,8 +1,54 @@
 package machine
 
-type address byte
+import (
+	"fmt"
+)
 
+type address uint
+
+// constants for some addresses of interest.
 const (
 	hVersion           address = 0
 	hStaticMemoryBegin address = 0x0E
 )
+
+type memory struct {
+	content []byte
+}
+
+// newMemory returns new memory initialised with contentToLoad.
+func newMemory(contentToLoad []byte) *memory {
+	return &memory{
+		content: contentToLoad,
+	}
+}
+
+// readByte returns the byte at address a, or panics if a is
+// out of bounds.
+func (m *memory) readByte(a address) uint8 {
+	m.boundsCheck(a)
+
+	return m.content[a]
+}
+
+// readWord returns the byte at address a concatenated with the
+// byte following it, or panics if a or a + 1 is out of bounds.
+func (m *memory) readWord(a address) uint16 {
+	m.boundsCheck(a)
+	m.boundsCheck(a + 1)
+
+	return uint16(m.content[a])<<8 | uint16(m.content[a+1])
+}
+
+// boundsCheck ensures that a is within the region of m's
+// content.
+func (m *memory) boundsCheck(a address) {
+	if int(a) > len(m.content) {
+		panic(fmt.Errorf("attempted to access address %x which is outside of initialised memory", a))
+	}
+}
+
+// size returns the length of the initialised memory.
+func (m *memory) size() int {
+	return len(m.content)
+}
