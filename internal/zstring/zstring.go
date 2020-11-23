abec1dca514a93737fd6a0c04a1930f6f2098e05
James Liu <james_liu@fastmail.com>
2020-11-23 21:43:03 +1100

    refactor: additional typing for `zstring`

--- internal/zstring/zstring.go
+++ internal/zstring/zstring.go
@@ -6,9 +6,11 @@ import (
 
 type ZChar uint8 // it's really 5 bits, but we can only go as low as 8 natively.
 
+type alphabet string
+
 // Alphabets begin at index 6.
-// The final alphabet is the A2 variation used by V1 of the Z-machine.
-var alphabet = []string{
+// The final alphabets is the A2 variation used by V1 of the Z-machine.
+var alphabets = []alphabet{
 	"abcdefghijklmnopqrstuvwxyz",
 	"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
 	" \n0123456789.,!?_#'\"/\\-:()",
@@ -17,11 +19,11 @@ var alphabet = []string{
 
 // Ztoa takes an array of Z-characters and returns its string representation.
 func Ztoa(chars []ZChar, version int) string {
-	currentAlphabet := alphabet[0]
+	currentAlphabet := alphabets[0]
 	var output strings.Builder
 
 	for _, char := range chars {
-		if char >= 6 {
+		if char >= 6 && char <= 31 {
 			output.WriteByte(currentAlphabet[char-6])
 		}
 	}
